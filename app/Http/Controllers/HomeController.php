5d3dca910051adebc4677c57f3babfffe61a6828
Bhaskar <bhaskar.bid18@gmail.com>
2023-12-25 01:59:38 +0530

    Added an homepage without login and register

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,9 @@ class HomeController extends Controller
             }
         }
     }
-
+    public function homepage(){
+        return view('home.homepage');
+    }
     /**
      * Show the form for creating a new resource.
      */
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
-
+});*/
+Route::get('/',[HomeController::class,'homepage']);
 /*Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');*/
