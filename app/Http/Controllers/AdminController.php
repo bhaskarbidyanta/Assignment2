f34cdcc2a1da82320799d8cd3c1b6f4ddbce30f4
Bhaskar <bhaskar.bid18@gmail.com>
2023-12-25 21:52:38 +0530

    Created add_post route and a function and a post_table

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class AdminController extends Controller
 {
     public function post_page(){
         return view('admin.post_page');
+    }
+
+    public function add_post(Request $request){
+        $post=new Post;
+        $post->title=$request->title;
+        $post->description=$request->description;
+        $post->save();
+        return redirect()->back();
     }
 }
--- resources/views/admin/post_page.blade.php
+++ resources/views/admin/post_page.blade.php
@@ -8,8 +8,17 @@
             font-weight: bold;
             text-align:center;
             padding:30 px;
+            color: white;
        }
 
+       .div_center{
+            text-align:center;
+            padding:30px;
+       }
+       .label{
+            display:inline-block;
+            width:200px;
+       }
     </style>
   </head>
   <body>
@@ -21,6 +30,30 @@
 
       <div class="page-content">
         <h1 class="post_title">Add Post</h1>
+        <div>
+            <form action="{{url('add_post')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="div_center">
+                    <label>Post Title</label>
+                    <input type="text" name="title">
+
+                </div>
+                <div class="div_center">
+                    <label>Post Description</label>
+                    <textarea name="description"></textarea>
+
+                </div>
+                <div class="div_center">
+                    <label>Add Image</label>
+                    <input type="file" name="image">
+
+                </div>
+                <div class="div_center">
+                    <input type="submit" class="btn btn-primary">
+                </div>
+            </form>
+
+        </div>
       </div>
         @include('admin.footer')
   </body>
