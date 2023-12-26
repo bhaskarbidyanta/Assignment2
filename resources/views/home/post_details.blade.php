7f6fb87f9818a8634a5c22281e7c8b761d0479ff
Bhaskar <bhaskar.bid18@gmail.com>
2023-12-26 13:08:45 +0530

    Display of details of Post as a page and some header modification

---
+++ resources/views/home/post_details.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <base href="/public">
+        @include('home.homecss');
+   </head>
+   <body>
+      <!-- header section start -->
+      <div class="header_section">
+         @include('home.header');
+
+      </div>
+
+      <div style="text-align:center" class="col-md-12">
+        <div><img style="padding:20px;height:400px;width:550px;margin:auto" src="/postimage/{{$post->image}}"></div>
+            <h4><b>{{$post->title}}</b></h4>
+            <h4>{{$post->description}}</h4>
+            <p>Post by <b>{{$post->name}}</b></p>
+
+     </div>
+
+      <!-- footer section start -->
+        @include('home.footer')
+   </body>
+</html>
